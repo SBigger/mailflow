59285486b07eb12d2f44c5b742a14359d01f7101
Roger <info@brsoft.ch>
2026-03-31 22:23:11 +0200

    - if logout -> spinner appear

--- src/lib/AuthContext.jsx
+++ src/lib/AuthContext.jsx
@@ -68,11 +68,14 @@ export function AuthProvider({ children }) {
   }
 
   async function signOut() {
+    setLoading(true);
     const { error} = await supabase.auth.signOut();
+    setLoading(false);
     if (error) {
       console.error("Logout fehlgeschlagen:", error);
       return;
     }
+
     setUser(null);
   }
 
